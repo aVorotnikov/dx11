91317d1b312bf1d37a3f3de9312171176094bd64
AV3pml30 <39947159+aVorotnikov@users.noreply.github.com>
2023-02-13 19:49:36 +0300

    Add task 1 code

---
+++ task2/main.cpp
@@ -0,0 +1,98 @@
+#include "renderer.h"
+
+#include <windows.h>
+
+#define __CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#define new DEBUG_NEW
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+     // Register the window class
+     const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+     WNDCLASS wc = {};
+
+     wc.lpfnWndProc = WindowProc;
+     wc.hInstance = hInstance;
+     wc.lpszClassName = CLASS_NAME;
+
+     RegisterClass(&wc);
+
+     // Create the window
+     HWND hWnd = CreateWindowEx(
+          0,                              // Optional window styles
+          CLASS_NAME,                     // Window class
+          L"DirectX11",                   // Window text
+          WS_OVERLAPPEDWINDOW,            // Window style
+
+          // Size and position
+          CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+          NULL,       // Parent window
+          NULL,       // Menu
+          hInstance,  // Instance handle
+          NULL        // Additional application data
+     );
+
+     if (NULL == hWnd)
+          return 0;
+
+     RECT rc;
+     rc.left = 0;
+     rc.right = Renderer::defaultWidth;
+     rc.top = 0;
+     rc.bottom = Renderer::defaultHeight;
+     AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, TRUE);
+     MoveWindow(hWnd, 100, 100, rc.right - rc.left, rc.bottom - rc.top, TRUE);
+
+     ShowWindow(hWnd, nCmdShow);
+
+     auto &renderer = Renderer::GetInstance();
+     if (!renderer.Init(hWnd))
+          return EXIT_FAILURE;
+
+     // Run the message loop
+     MSG msg = {};
+     HACCEL hAccelTable = LoadAccelerators(hInstance, L"");
+     bool exit = false;
+     while (!exit)
+     {
+          if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) > 0)
+          {
+               if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+               {
+                    TranslateMessage(&msg);
+                    DispatchMessage(&msg);
+               }
+               if (WM_QUIT == msg.message)
+                    exit = true;
+          }
+          renderer.Render();
+     }
+
+     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+     switch (uMsg)
+     {
+          case WM_DESTROY:
+               PostQuitMessage(0);
+               return 0;
+
+          case WM_SIZE:
+          {
+               RECT rc;
+               GetClientRect(hWnd, &rc);
+               Renderer::GetInstance().Resize(rc.right - rc.left, rc.bottom - rc.top);
+          }
+
+          return 0;
+     }
+     return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
---
+++ task2/renderer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <d3d11.h>
+#include <dxgi.h>
+#include <windows.h>
+
+class Renderer
+{
+public:
+     static Renderer &GetInstance();
+     Renderer(const Renderer &) = delete;
+     Renderer(Renderer &&) = delete;
+
+     ~Renderer();
+     void CleanAll();
+
+     bool Init(const HWND hWnd);
+     bool Render();
+     bool Resize(const unsigned width, const unsigned height);
+
+     static constexpr const unsigned defaultWidth = 1280;
+     static constexpr const unsigned defaultHeight = 720;
+
+private:
+     Renderer();
+
+     ID3D11Device *pDevice_;
+     ID3D11DeviceContext *pDeviceContext_;
+     IDXGISwapChain *pSwapChain_;
+     ID3D11RenderTargetView *pBackBufferRTV_;
+
+     unsigned width_;
+     unsigned height_;
+};
